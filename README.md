77bfc1ab0dc4f2116864a534a7d458131eb8f28b
shataojin <stj2062287@gmail.com>
2021-05-31 14:33:54 -0400

    添加项目文件。

---
+++ README.md
@@ -0,0 +1,4 @@
+# GAME3001-M2021-assignment-1
+
+Demo Project for GAME3001-M2021-assignment-1
+
---
+++ src/PlayScene.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef __PLAY_SCENE__
+#define __PLAY_SCENE__
+
+#include "Obstacle.h"
+#include "Scene.h"
+#include "StarShip.h"
+#include "Target.h"
+
+
+class PlayScene : public Scene
+{
+public:
+	PlayScene();
+	~PlayScene();
+
+	// Scene LifeCycle Functions
+	virtual void draw() override;
+	virtual void update() override;
+	virtual void clean() override;
+	virtual void handleEvents() override;
+	virtual void start() override;
+
+	//void moveStarShip() const;
+private:
+	// IMGUI Function
+	void GUI_Function();
+	std::string m_guiTitle;
+	
+	glm::vec2 m_mousePosition;
+
+	// Game Objects
+	Target* m_pTarget;
+	StarShip* m_pStarShip;
+	Obstacle* m_pObstacle;
+
+	// UI Controls
+	bool m_bToggleSeek;
+};
+
+#endif /* defined (__PLAY_SCENE__) */
